d99ebf4b5d8cbf09ae0b0c23b58740c629372384
Михаил <ya.mikushoff@yandex.ru>
2017-06-26 20:00:51 +0300

    сайт загружен

--- README.md
+++
@@ -1,3 +0,0 @@
-# Travelline summer practise
-
-[Cайт]( http://cg77613.tmweb.ru) <http://cg77613.tmweb.ru>
--- src/js/_.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    function setTheHeightHeroSlider() {
-        var ORBIT_CONTAINER_HEIGHT = 400;
-        var orbitContainer  = document.querySelector('#js-orbit-container');
-
-        orbitContainer.style.height = ORBIT_CONTAINER_HEIGHT;
-    }
-};
